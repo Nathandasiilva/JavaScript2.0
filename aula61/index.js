8829efe3e1e48f521f3e6167a536277117bb7e08
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-08-28 23:43:09 -0300

    update

---
+++ aula61/index.js
@@ -0,0 +1,15 @@
+// Pop remove do final e Shift do começo
+//const nomes = ['Nathan', 'Maria', 'Joana',];
+//const novo = nomes.slice(1, -1);
+//console.log(nomes);
+
+const nomes = ('Nathan', 'da', 'Silva');
+//const nome = nomes.split(' ');
+const nome = nomes.join(' ');
+console.log(nome);
+
+
+
+
+
+//nomes.push('João', 'Icaro');
---
+++ aula62/index.js
@@ -0,0 +1,11 @@
+//               -5        -4       -3      -2       -1
+//                0         1        2       3        4
+const nomes = ['Nathan', 'Maria', 'Joana', 'João', 'Icaro'];
+// nomes.splice(indice, delete, ele1, ele2, ele3);
+// pop
+//const removidos = nomes.splice(3, 2, 'Gabriel', 'Mauricio');
+console.log(nomes, removidos);
+
+
+
+
