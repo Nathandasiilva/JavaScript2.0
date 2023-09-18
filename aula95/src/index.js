c834f79937a625c68788f5a339e8c50537767a21
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-09-18 16:45:53 -0300

    Update

---
+++ aula95/src/index.js
@@ -0,0 +1,26 @@
+import Pessoa, {nome, sobreNome} from './modulo1';
+
+const p1 = new Pessoa(nome, sobreNome);
+console.log(p1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const p1 = new Pessoa('Nathan', 'Silva')
+//console.log(p1)
+
+
+
+//console.log( nome, sobreNome, idade);
+//console.log(soma(2, 2));
---
+++ aula95/src/modulo1.js
@@ -0,0 +1,31 @@
+export const nome = 'Nathan';
+export const sobreNome = 'Silva';
+export const idade = 20;
+
+export function soma (x, y) {
+    return x + y;
+}
+
+export default class Pessoa {
+    constructor(nome, sobreNome,) {
+        this.nome = nome;
+        this.sobreNome = sobreNome;
+    }
+}
+
+
+
+
+
+/*
+export class Pessoa {
+    constructor(nome, sobreNome) {
+        this.nome = nome;
+        this.sobreNome = sobreNome;
+    }
+} */
+
+
+
+
+//export {nome, sobreNome, idade, soma};
