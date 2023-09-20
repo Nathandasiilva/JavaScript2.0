3b3f675b8d4e34fc8c95609d9ab042e203c882c2
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-09-20 20:35:13 -0300

    Update

--- aula96/src/main.js
+++ aula96/src/main.js
@@ -2,11 +2,10 @@ import GeraCPF from './modules/GeraCPF';
 
 import './assets/css/style.css';
 
-(function () {
+(function() {
     const gera = new GeraCPF();
     const cpfGerado = document.querySelector('.cpf-gerado');
     cpfGerado.innerHTML = gera.geraNovoCpf();
-
-})();
+  })();
 
 
