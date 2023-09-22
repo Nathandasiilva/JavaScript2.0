62ab6dd6f8ce705a8d762d324ae8b40f9f525d7c
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-09-21 23:32:08 -0300

    Update

--- aula97/src/modules/formGeraSenha.js
+++ aula97/src/modules/formGeraSenha.js
@@ -2,26 +2,26 @@ import geraSenha from './geradores';
 
 const senhaGerada = document.querySelector('.senha-gerada');
 const qtdCaracteres = document.querySelector('.qtd-caracteres');
-const chkMainusculas = document.querySelector('.chk-mainusculas');
-const chkMinusculas  = document.querySelector('.chk-minusculas');
+const chkMaiusculas = document.querySelector('.chk-maiusculas');
+const chkMinusculas = document.querySelector('.chk-minusculas');
 const chkNumeros = document.querySelector('.chk-numeros');
 const chkSimbolos = document.querySelector('.chk-simbolos');
-const gerarSenha = document.querySelector('.gera-senha')
-
+const gerarSenha = document.querySelector('.gerar-senha');
 
 export default () => {
-    gerarSenha.addEventListener('click', () => {
-        senhaGerada.innerHTML = 
-    });
+  gerarSenha.addEventListener('click', () => {
+    senhaGerada.innerHTML = gera();
+  });
 };
 
 function gera() {
-    const senha = geraSenha(
-        qtdCaracteres.value,
-        chkMainusculas.checked,
-        chkMinusculas.checked,
-        chkNumeros.checked,
-        chkSimbolos.checked
-    );
-    return senha || 'Nada Selecionado.';
+  const senha = geraSenha(
+    qtdCaracteres.value,
+    chkMaiusculas.checked,
+    chkMinusculas.checked,
+    chkNumeros.checked,
+    chkSimbolos.checked
+  );
+
+  return senha || 'Nada selecionado.';
 }
