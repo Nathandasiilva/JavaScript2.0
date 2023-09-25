36799ed5daf6f805a1963a285fa251667d745b28
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-09-25 14:21:17 -0300

    Update

---
+++ node/aula6/serve.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+//          CRIAR    LET  ATUALIZAR APAGAR
+// CRUD -> CREATE,  READ,  UPDATE,  DELETE
+//          POST     GET     PUT    DELETE
+
+app.get('/', (req, res) => {
+    res.send(`
+    <form action="/" method="POST">
+    Nome: <input type"text" name="nome"
+    <button>ENVIAR</button>`)
+});
+
+app.post('/', (req, res) => {
+    res.send('Recebi a formulario')
+})
+
+app.get('/contato', (req, res) => {
+    res.send('Obrigado por entra em contato')
+})
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando a porta 3000');
+});
