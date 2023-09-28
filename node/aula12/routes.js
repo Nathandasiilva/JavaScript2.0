25f20f642cde0596147979104701215788b47a47
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-09-27 22:45:02 -0300

    Update

---
+++ node/aula12/routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const route = express.Router();
+const homeController = require('./src/controllers/homeController');
+const contatoController = require('./src/controllers/contatoController');
+
+// Rotas da home
+route.get('/', homeController.paginaIncial);
+route.post('/', homeController.trataPost)
+
+// Rotas de contatos
+
+route.get('/contato', contatoController.paginaInicial);
+
+
+module.exports = route;
