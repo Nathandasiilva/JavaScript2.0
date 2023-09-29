b06f02407081b9679fa28bdfbc18c158fedd4c37
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-09-29 17:52:56 -0300

    Update

---
+++ node/aula16/src/middlewares/middleware.js
@@ -0,0 +1,8 @@
+module.exports = (req, res, next) =>{
+    res.locals.umaVarialvelLocal = 'Este é o valor da variavel local.'
+    console.log();
+    console.log('Passei no middleware global');
+    console.log();
+exports.outro
+    next();
+}; 
