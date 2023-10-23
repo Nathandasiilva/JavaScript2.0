f464cb708a361bfa6216173f8408c1502def81aa
Nathan da Silva <nathan.silva1043@gmail.com>
2023-10-23 13:35:37 -0300

    Update

--- API_REST/src/controllers/HomeControllers.js
+++ API_REST/src/controllers/HomeControllers.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Nathan',
+      sobrenome: 'Silva',
+      email: 'nathan@gmail.com',
+      idade: 21,
+      peso: 85,
+      altura: 1.7,
     });
+    res.json(novoAluno);
   }
 }
 
