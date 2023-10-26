0084766af74d05f199367267e6601bc8863dc658
Nathan da Silva <nathan.silva1043@gmail.com>
2023-10-25 22:08:50 -0300

    Update

--- API_REST/src/routes/userRoutes.js
+++ API_REST/src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import userController from '../controllers/UserControllers';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const routes = new Router();
 
 routes.post('/', userController.store);
-routes.get('/', userController.index);
+routes.get('/', loginRequired, userController.index);
 routes.get('/:id', userController.show);
 routes.put('/:id', userController.update);
 routes.delete('/:id', userController.delete);
