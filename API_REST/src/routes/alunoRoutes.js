ca633462190bcb035131e61d95a0bb7fdc727269
Nathan da Silva <nathan.silva1043@gmail.com>
2023-10-31 21:58:16 -0300

    Update

--- API_REST/src/routes/alunoRoutes.js
+++ API_REST/src/routes/alunoRoutes.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import alunoController from '../controllers/AlunoControllers';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const routes = new Router();
 
 routes.get('/', alunoController.index);
+routes.post('/', loginRequired, alunoController.store);
+routes.put('/id', loginRequired, alunoController.update);
+routes.get('/id', alunoController.show);
+routes.delete('/id', loginRequired, alunoController.delete);
 
 export default routes;
