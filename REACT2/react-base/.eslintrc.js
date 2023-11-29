635d07190645bac123812ab95131c9982ff62048
Nathan da Silva <nathan.silva1043@gmail.com>
2023-11-29 20:15:30 -0300

    Update

--- REACT2/react-base/.eslintrc.js
+++ REACT2/react-base/.eslintrc.js
@@ -2,6 +2,48 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "react-hooks"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/state-in-constructor": 0,
+    "react/forbid-porp-types": 0
+  },
+};
+
+
+
+
+/*
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
   },
   extends: ["airbnb", "prettier", "prettier/react"],
   overrides: [
@@ -29,3 +71,4 @@ module.exports = {
     "react-hooks/exhaustive-deps": "warn",
   },
 };
+*/
