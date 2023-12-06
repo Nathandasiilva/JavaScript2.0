171228f71742cc0b86bafbabac5c933b3d1db35b
Nathan da Silva <nathan.silva1043@gmail.com>
2023-12-05 22:22:21 -0300

    Update

--- REACT2/react-base/src/store/modules/example/action.js
+++ REACT2/react-base/src/store/modules/example/action.js
@@ -2,6 +2,18 @@ import * as types from '../types';
 
 export function clicaBotao() {
   return {
-    type: types.BOTAO_CLICADO,
+    type: types.BOTAO_CLICADO_REQUEST,
+  };
+}
+
+export function clicaBotao() {
+  return {
+    type: types.BOTAO_CLICADO_SUCCESS,
+  };
+}
+
+export function clicaBotao() {
+  return {
+    type: types.BOTAO_CLICADO_FAILURE,
   };
 }
--- REACT2/react-base/src/store/modules/example/reducer.js
+++ REACT2/react-base/src/store/modules/example/reducer.js
@@ -1,12 +1,25 @@
+import * as types from '../types';
+
 const initialState = {
   botaoClicando: false,
 };
 
 const reducer = (state = initialState, action) {
   switch (action.type) {
-    case 'BOTAO_CLICADO': {
+    case types.BOTAO_CLICADO_SUCCESS: {
+      console.log('Sucesso');
       const newState = { ...state };
       newState.botaoClicando = !newState.botaoClicando;
+      return newState;
+    }
+
+    case types.BOTAO_CLICADO_FAILURE: {
+      console.log('Deu erro =(');
+      return newState;
+    }
+
+    case types.BOTAO_CLICADO_REQUEST: {
+      console.log('Estou fazendo a requisição');
       return newState;
     }
 
